99b6d8e96502546ec4b4a0857d7ac2c9240e9344
mehedi-hasan-2302 <mehedi.h2302@gmail.com>
2024-02-16 21:13:22 +0600

    buttons modified and js file added

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function PageTransitions(){
+    // button click active class
+
+    for(let i=0; i<sectBtn.length; i++)
+    {
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+}
+
+PageTransitions();
